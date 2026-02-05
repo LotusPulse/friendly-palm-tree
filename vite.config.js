a8f984eb5c92d4cd187ff3764b4e2a13edc32b7e
Kermo Raja <kermo.raja@elisa.ee>
2026-02-06 00:43:43 +0200

    Add Buffer polyfill for Cloudflare

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,15 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [
+		sveltekit(),
+		nodePolyfills({
+			include: ['buffer'],
+			globals: {
+				Buffer: true,
+			},
+		}),
+	]
 });
